53b5aea22d8f31fed2865b706060391036c69438
AlexRiveron <47648183+AlexRiveron@users.noreply.github.com>
2019-02-26 22:14:37 -0500

    Update Q3.vhd

--- Q3.vhd
+++ Q3.vhd
@@ -2,9 +2,9 @@
 -- Company: 
 -- Engineer: 
 -- 
--- Create Date: 02/26/2019 09:16:50 PM
+-- Create Date: 02/21/2019 02:23:28 PM
 -- Design Name: 
--- Module Name: fancy_counter - Behavioral
+-- Module Name: Q3 - Behavioral
 -- Project Name: 
 -- Target Devices: 
 -- Tool Versions: 
@@ -19,73 +19,91 @@
 ----------------------------------------------------------------------------------
 
 
+-- library declaration
 library IEEE;
-use IEEE.STD_LOGIC_1164.ALL;
-use ieee.numeric_std.ALL;
+use IEEE.std_logic_1164.all;
+-- entity
 
--- Uncomment the following library declaration if using
--- arithmetic functions with Signed or Unsigned values
---use IEEE.NUMERIC_STD.ALL;
+entity mux2t1 is
+    Port ( A,B: in STD_LOGIC_VECTOR(7 downto 0);
+           SEL : in STD_LOGIC;
+           M_out : out STD_LOGIC_VECTOR(7 downto 0));
+end mux2t1;
 
--- Uncomment the following library declaration if instantiating
--- any Xilinx leaf cells in this code.
---library UNISIM;
---use UNISIM.VComponents.all;
-
-entity fancy_counter is
-    Port ( clk : in STD_LOGIC;
-           clk_en : in STD_LOGIC;
-           dir : in STD_LOGIC;
-           en : in STD_LOGIC;
-           ld : in STD_LOGIC;
-           rst : in STD_LOGIC;
-           updn : in STD_LOGIC;
-           val : in STD_LOGIC_VECTOR (3 downto 0);
-           cnt : inout STD_LOGIC_VECTOR (3 downto 0));
-end fancy_counter;
-
-architecture Behavioral of fancy_counter is
-
-signal temp: std_logic := '0';
-
-begin
-    process(clk)
+architecture mux2t1 of mux2t1 is
     begin
-        if(rising_edge(clk)) then
-            if (en = '1') then
-                if (rst = '0') then
-                    if (clk_en = '1') then
-                        if (updn ='1') then
-                            temp <= dir;
-                        end if;
-                        if (ld = '1') then
-                            cnt <= val;
-                        else if (temp = '1') then
-                            if (cnt = val) then
-                                cnt <= "0000";
-                            else 
-                                cnt <= STD_LOGIC_VECTOR (unsigned(cnt) + 1);
-                            end if;
-                        else if( temp = '0') then
-                            if (cnt = "0000") then
-                                cnt <= val;
-                            else 
-                                cnt <= STD_LOGIC_VECTOR (unsigned(cnt) - 1);
-                            end if;
-                        end if;
-                        end if;
-                        end if;
-                    end if;
-                else if (rst = '1') then
-                    cnt <= "0000";
-                    if (clk_en = '1') then
-                        if (updn = '1') then
-                            temp <= dir;
-                        end if;
-                    end if;
-                end if;
-                end if;
-            end if;
-        end if;
-    end process;
-end Behavioral;
+  with SEL select
+  M_out <= A when '0',
+			B when '1',
+			(others => '0') when others;
+end mux2t1;
+
+-- library declaration
+library IEEE;
+use IEEE.std_logic_1164.all;
+-- entity
+
+entity reg8 is
+	Port ( REG_IN: in STD_LOGIC_VECTOR(7 downto 0);
+			LD, CLK : in std_logic;
+			REG_OUT: out std_logic_vector(7 downto 0));
+end reg8;
+architecture reg8 of reg8 is
+begin 
+	reg: process(CLK)
+	begin
+		if (rising_edge(CLK)) then
+			if (LD = '1') then 
+			REG_OUT <= REG_IN;
+		end if;
+	end if;
+end process;
+end reg8;
+--map circuitry
+
+-- library declaration
+library IEEE;
+use IEEE.std_logic_1164.all;
+-- entity
+
+entity ckt_q3 is
+	port (X,Y: in std_logic_vector(7 downto 0);
+			S0, S1: in std_logic;
+			LDA, LDB, CLK: in std_logic;
+			reg_a, reg_b: inout STD_LOGIC_VECTOR(7 downto 0));
+end ckt_q3;
+architecture q3_structure of ckt_q3 is 
+	component mux2t1
+		Port ( A,B: in STD_LOGIC_VECTOR(7 downto 0);
+				SEL: in STD_LOGIC;
+				M_out: out STD_LOGIC_VECTOR(7 downto 0));
+	end component;
+	component reg8
+		Port(  REG_IN: in STD_LOGIC_VECTOR(7 downto 0);
+			LD, CLK : in std_logic;
+			REG_OUT: out std_logic_vector(7 downto 0));
+	end component;
+
+		signal mux_result1, mux_result2 : std_logic_vector (7 downto 0);
+begin
+    ra: reg8
+	port map ( REG_IN => mux_result1,
+	           LD => LDA,
+	           CLK => CLK,
+	           REG_OUT => reg_a);
+	rb: reg8
+	port map ( REG_IN => reg_a,
+				LD => LDB,
+				CLK => CLK,
+				REG_OUT => reg_b);
+	Ma: mux2t1
+	port map (  A => X,
+				B => reg_b,
+				SEL => S1,
+				M_out => mux_result1);
+	Mb: mux2t1
+    port map (  A => reg_a,
+                B => Y,
+                SEL => S1,
+                M_out => mux_result2);
+end q3_structure;
